e58d2b92dd315c54d57a44486c3dc592ebe8922e
Franz Gusto <fkrekeler@gmail.com>
2022-10-07 21:42:35 -0400

    first init

---
+++ resources.md
@@ -0,0 +1,27 @@
+https://docs.metamask.io/guide/snaps-rpc-api.html#restricted-methods
+https://inspector.open-rpc.org
+https://github.com/Montoya/address-book-snap-tutorial#readme
+https://github.com/ritave/snap-passwordManager
+https://docs.metamask.io/guide/snaps-development-guide.html#developing-a-snap
+https://github.com/MetaMask/snaps-skunkworks/discussions/821
+
+
+
+// Extension: Metamask
+// Sandbox 1: Snap
+Url: OpenSea
+
+Dapp: Snap Interaction
+
+// Etension: FooBar
+
+FooBar -> Injects button into OpenSea.
+User -> Clicks the button on OpenSea
+Button -> Sends wallet_invokeSnap rpc call
+Snap -> requestes private keys from Metamask
+Snap -> sends the transaction to infura using fetch
+
+
+//Batch transaction
+
+
---
+++ snaps/notifications/src/index.js
@@ -0,0 +1,88 @@
+/**
+ * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
+ *
+ * @param {object} args - The request handler args as object.
+ * @param {string} args.origin - The origin of the request, e.g., the website
+ * that invoked the snap.
+ * @param {JsonRpcRequest<unknown[] | Record<string, unknown>>} args.request - A
+ * validated JSON-RPC request object.
+ * @returns {boolean} `null` if the request succeeded.
+ * @throws If the request method is not valid for this snap.
+ * @throws If the `snap_notify` call failed.
+ */
+
+
+//https://github.com/Montoya/gas-fee-snap/blob/main/packages/snap/src/index.ts
+
+module.exports.onRpcRequest = async (data) => {
+  const { origin, request } = data
+  console.log('onRpcRequest', JSON.stringify(data,null,2));
+  console.log(JSON.stringify(request,null,2))
+  console.log(JSON.stringify(orign,null,2))
+  switch (request.method) {
+    case 'inApp':
+      return wallet.request({
+        method: 'snap_notify',
+        params: [
+          {
+            type: 'inApp',
+            message: `Hello, ${origin}!`,
+          },
+        ],
+      });
+    case 'native':
+      return wallet.request({
+        method: 'snap_notify',
+        params: [
+          {
+            type: 'native',
+            message: `Hello, ${origin}!`,
+          },
+        ],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+};
+
+
+
+module.exports.onConfirm = async (data) => {
+  console.log("ONCOFIRM")
+  console.log(data)
+
+}
+module.exports.onTransaction = async (data) => {
+  console.log('onTransaction', JSON.stringify(data,null,2));
+  const {transaction} = data
+  console.log("window2")
+  console.log(window)
+  // console.log(JSON.stringify(window.wallet,null,2))
+  // async 
+  // const todos = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // .then(response => response.json())
+  console.log("todos")
+  console.log(JSON.stringify(todos,null,2))
+  console.log("transaction")
+  console.log(JSON.stringify(transaction,null,2))
+  return {
+    insights: { score: 43, "Is contract verified on Etherscan?": "Yes"},
+
+  };
+};
+
+
+
+module.exports.keyring = {
+  handleRequest: async (data) => {
+    console.log(JSON.stringify(data))
+    const {request} = data
+    switch (request.method) {
+      case "eth_accounts":
+        return getAccounts();
+      case "gnosis_watchSafe":
+        return watchSafe(request.params[0]);
+      /* ... */
+    }
+  },
+};
